29958b19c99ba4dd7c0ebe7bd9e5c94e813482e0
Fernando <fernu292@gmail.com>
2023-12-27 17:54:51 -0600

    Adding Button class

---
+++ src/Ui/Components/Button.h
@@ -0,0 +1,15 @@
+//
+// Created by fernu on 12/27/23.
+//
+
+#ifndef BANCO_PRUEBAS_BUTTON_H
+#define BANCO_PRUEBAS_BUTTON_H
+
+#include <QPushButton>
+
+class Button {
+
+};
+
+
+#endif //BANCO_PRUEBAS_BUTTON_H
