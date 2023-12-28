1c4af050d0af92707d29f1aaab8a748209a4dfe5
Fernando <fernu292@gmail.com>
2023-12-28 02:36:54 -0600

    Delete build folder

--- cmake-build-debug/CMakeFiles/Banco_Pruebas.dir/Banco_Pruebas_autogen/mocs_compilation.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/Banco_Pruebas.dir/Banco_Pruebas_autogen/mocs_compilation.cpp.o: \
- /home/fernu/Projects/Banco_Pruebas/cmake-build-debug/Banco_Pruebas_autogen/mocs_compilation.cpp \
- /usr/include/stdc-predef.h
